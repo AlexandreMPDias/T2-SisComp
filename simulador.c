0c8e755957fe6fab148e0542d5af7d29986c7fbc
Alexandre Dias <alexandrempdias@icloud.com>
2017-12-06 22:05:31 -0200

    asda

--- simulador.c
+++ simulador.c
@@ -156,6 +156,7 @@ void create_process(char* arquivo, u_int sleeper){
 	while(fscanf(file,"%x %c ", &addr, &rw) != 0){
 		i = to_side(addr, _left_);
 		o = to_side(addr, _right_);
+<<<<<<< HEAD
 		if(!trans(pid,i,o,rw)){
 			while(isLocked_info()){
 				sleep_nano(sleeper);
@@ -164,6 +165,9 @@ void create_process(char* arquivo, u_int sleeper){
 			kill(ppid(), SIGUSR1);
 			raise(SIGSTOP);
 		}
+=======
+		trans(pid,i,o,rw));
+>>>>>>> 9407471576f2f76430fc4a3d6aa324d71f8de84c
 	}
 	fclose(file);
 }
@@ -190,22 +194,40 @@ void sig_handler(int signal){
 }
 
 bool trans(pid_t pid, u_short i, u_short offset, char rw){
-	//abrir mem. compartilhada do processo
-	//percorrer table de memoria compartilhada checando se possui mapeamento
 	int 	segmento = get_segmento(pid);
+	int segment;
+	Fault_Info information;
+
 	u_int	entry = look_table(segmento, i, _left_);
 	long	sleeper = (long)(pid & 0x00000FFF);
 
 
 	if(entry == _max_){
+<<<<<<< HEAD
+		segment = EH_shmget(fault_key, sizeof(Fault_Info), S_IRUSR | S_IWUSR);
+		information=EH_shmat(segment,0,0);
+		//checa aqui se precisa esperar ou pode salvar
+		information.pid=getpid();
+		information.virtual_page;
+=======
 		//se nao, avisa o GM que houve pagefault
 		//salva o numero do processo requerinte e pagina virtual nao mapeada em uma outra memoria compartilhada(precisa ser criada pelo processo pai)
+<<<<<<< HEAD
 		// if(isLocked_info()){
 		// 	sleep_nano(sleeper);
 		// }
 		// unlock_info(pid, i);
 		// kill(ppid(), SIGUSR1);
 		// raise(SIGSTOP);
+=======
+		if(isLocked_info()){
+			sleep_nano(sleeper);
+		}
+		unlock_info(pid, i);
+>>>>>>> f1ed1399d3c4bdff3ad6bba2923bb1db84b61322
+		kill(ppid(), SIGUSR1);
+		raise(SIGSTOP);
+>>>>>>> 9407471576f2f76430fc4a3d6aa324d71f8de84c
 		return false;
 	}
 	else {
